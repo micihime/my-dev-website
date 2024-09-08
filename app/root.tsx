a15e2ba874a644d0bb7c05b5ad1b311dd9f3ddbc
Miroslava Hreskova <hroncovam@yahoo.com>
2024-09-08 17:32:10 +0100

    Adding Header and Footer to layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,7 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration, } from "@remix-run/react";
 import './styles.css'
+import Header from "./routes/components/Header";
+import Footer from "./routes/components/Footer";
 
 export default function App() {
   return (
@@ -11,7 +13,10 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <Header />
         <Outlet />
+        <Footer />
+
         <ScrollRestoration />
         <Scripts />
       </body>
