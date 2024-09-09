e89f7817fc5b7b8eff0d276275a31206c9ab4786
Miroslava Hreskova <hroncovam@yahoo.com>
2024-09-09 12:41:44 +0100

    Removing unnecessary divs in About route

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -10,38 +10,31 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <div className="values-list">
-        <div className="content">
-          <h2>My values</h2>
-          <ul className="flexi">
-            <li>
-              <IconSchool size={50} />
-              <h3>Continuous Learning:</h3> 
-              <p>
-                I&apos;m always eager to learn new tools, frameworks, and techniques to stay on top of the ever-changing tech landscape.
-              </p>
-            </li>
-            <li>
-              <IconCode size={50} />
-              <h3>Quality:</h3> 
-              <p>
-                I believe in writing clean, maintainable code and creating applications that not only work well but also deliver a seamless user experience.
-              </p>
-            </li>
-            <li>
-              <IconBulb size={50} />
-              <h3>Creativity:</h3> 
-              <p>
-                I enjoy finding creative solutions to complex problems.
-              </p>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <p>
-        I&apos;m always open to exciting new opportunities and collaborations. Feel free to get in touch if you&apos;d like to work together or simply share ideas.
-      </p>
+    <div className="values-list content">
+      <h1>My Core Values</h1>
+      <ul className="flexi">
+        <li>
+          <IconSchool size={50} />
+          <h3>Continuous Learning:</h3> 
+          <p>
+            I&apos;m always eager to learn new tools, frameworks, and techniques to stay on top of the ever-changing tech landscape.
+          </p>
+        </li>
+        <li>
+          <IconBulb size={50} />
+          <h3>Creativity:</h3> 
+          <p>
+            Creativity is at the heart of problem-solving. While coding may seem purely technical, I view it as a form of art where there are multiple ways to approach a solution, and the challenge is finding the one that&apos;s both elegant and efficient.
+          </p>
+        </li>
+        <li>
+          <IconCode size={50} />
+          <h3>Quality:</h3> 
+          <p>
+            I believe in writing clean, maintainable code and creating applications that not only work well but also deliver a seamless user experience.
+          </p>
+        </li>
+      </ul>
     </div>
   );
 }
