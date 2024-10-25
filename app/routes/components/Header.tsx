2f0a1ca991edd5d44a570e4fa06f61de2dccf31c
Miroslava Hreskova <hroncovam@yahoo.com>
2024-10-25 16:20:31 +0100

    Removing Home and adding Home link to the logo

--- app/routes/components/Header.tsx
+++ app/routes/components/Header.tsx
@@ -1,26 +1,36 @@
-import { useState } from 'react'
-import 'app/styles.css'
+import { useState } from "react";
+import "app/styles.css";
 
 export default function Header() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    return (
-        <div className="header">
-            <div className="content flexi">
-                <div className="logo">Mitchie</div>
-                <button 
-                    className="mobile-menu-button"
-                    onClick={() => setIsMenuOpen(!isMenuOpen)}
-                >
-                    ☰
-                </button>
-                <ul className={`navbar flexi ${isMenuOpen ? 'menu-open' : ''}`}>
-                    <li><a href="/values">My Values</a></li>
-                    <li><a href="/skills">Skills</a></li>
-                    <li><a href="/portfolio">Portfolio</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </div>
-        </div>
-    )
+  return (
+    <div className="header">
+      <div className="content flexi">
+        <a href="/">
+          <div className="logo">Mitchie</div>
+        </a>
+        <button
+          className="mobile-menu-button"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          ☰
+        </button>
+        <ul className={`navbar flexi ${isMenuOpen ? "menu-open" : ""}`}>
+          <li>
+            <a href="/values">My Values</a>
+          </li>
+          <li>
+            <a href="/skills">Skills</a>
+          </li>
+          <li>
+            <a href="/portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
