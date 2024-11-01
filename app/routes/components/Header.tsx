b635326010f65c43d7eb6e892c38dca5ae0bbfdf
Miroslava Hreskova <hroncovam@yahoo.com>
2024-11-01 12:00:50 +0000

    Removing Navigation.tsx and its css module, moving it to Header

--- app/routes/components/Header.tsx
+++ app/routes/components/Header.tsx
@@ -1,15 +1,51 @@
 import "app/styles.css";
-import Nav from "./Navigation";
+import Hamburger from "./Hamburger";
+import { useState } from "react";
 import styles from "./Header.module.css";
 
 export default function Header() {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
+
+  const navigationClass = `${styles.navigation} ${
+    hamburgerOpen ? styles.navigationOpen : ""
+  }`;
+
   return (
     <div className={styles.header}>
       <div className={`${styles.content} flexi`}>
         <div className={styles.logo}>
           <a href="/">Mitchie</a>
         </div>
-        <Nav />
+        <div className={navigationClass}>
+          <ul className={styles.navbar}>
+            <li>
+              <a href="/skills">Skills</a>
+            </li>
+            <li>
+              <a href="/portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+          <button
+            className={styles.hamburger}
+            onClick={toggleHamburger}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" || e.key === " ") {
+                toggleHamburger();
+              }
+            }}
+            aria-label="Menu"
+            aria-expanded={hamburgerOpen}
+          >
+            <Hamburger isOpen={hamburgerOpen} />
+          </button>
+        </div>
       </div>
     </div>
   );
