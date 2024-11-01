6bf004a635b7a4dd558070f9eed5f5318747bf95
Miroslava Hreskova <hroncovam@yahoo.com>
2024-11-01 15:00:10 +0000

    Improving the hamburger menu

--- app/routes/components/Hamburger.tsx
+++ app/routes/components/Hamburger.tsx
@@ -2,12 +2,10 @@ import styles from "./Hamburger.module.css";
 
 export default function Hamburger({ isOpen }: { isOpen: boolean }) {
   return (
-    <>
-      <div className={`${styles.hamburger} ${isOpen ? styles.open : ""}`}>
-        <div className={`${styles.burger} ${styles.burger1}`} />
-        <div className={`${styles.burger} ${styles.burger2}`} />
-        <div className={`${styles.burger} ${styles.burger3}`} />
-      </div>
-    </>
+    <div className={`${styles.hamburger} ${isOpen ? styles.open : ""}`}>
+      <div className={`${styles.burger} ${styles.burger1}`} />
+      <div className={`${styles.burger} ${styles.burger2}`} />
+      <div className={`${styles.burger} ${styles.burger3}`} />
+    </div>
   );
 }
