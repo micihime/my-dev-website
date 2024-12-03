4322e9bd580bea8badc50000dc1885347784e1c5
Miroslava Hreskova <hroncovam@yahoo.com>
2024-12-03 15:21:13 +0100

    Adding footer links to github and LI profile

--- app/routes/components/Footer.tsx
+++ app/routes/components/Footer.tsx
@@ -1,9 +1,18 @@
 import styles from "./Footer.module.css";
+import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
 
 export default function Index() {
   return (
     <div className={styles.footer}>
-      <p>Mitchie &hearts;</p>
+      <p>
+        Mitchie{" "}
+        <a href="https://www.linkedin.com/in/miroslava-hre%C5%A1kov%C3%A1-bbb5939a/">
+          <IconBrandGithub size={18} />
+        </a>{" "}
+        <a href="https://github.com/micihime">
+          <IconBrandLinkedin size={18} />
+        </a>
+      </p>
     </div>
   );
 }
