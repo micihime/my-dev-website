a672abf06a7a23185505b3d7bfc5522f932b514b
Miroslava Hreskova <hroncovam@yahoo.com>
2024-12-03 16:48:22 +0100

    Open links in new tab

--- app/routes/components/Footer.tsx
+++ app/routes/components/Footer.tsx
@@ -1,5 +1,9 @@
 import styles from "./Footer.module.css";
-import { IconBrandGithub, IconBrandLinkedin, IconMail } from "@tabler/icons-react";
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconMail,
+} from "@tabler/icons-react";
 
 export default function Index() {
   return (
@@ -8,13 +12,25 @@ export default function Index() {
         <span>Mitchie</span>
       </div>
       <div className={styles.socialIcons}>
-        <a href="mailto:mitchie.code@gmail.com">
+        <a
+          href="mailto:mitchie.code@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <IconMail size={18} />
         </a>
-        <a href="https://www.linkedin.com/in/miroslava-hre%C5%A1kov%C3%A1-bbb5939a/">
+        <a
+          href="https://www.linkedin.com/in/miroslava-hre%C5%A1kov%C3%A1-bbb5939a/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <IconBrandGithub size={18} />
         </a>
-        <a href="https://github.com/micihime">
+        <a
+          href="https://github.com/micihime"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <IconBrandLinkedin size={18} />
         </a>
       </div>
