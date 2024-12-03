893f1495f1370ef537eb9a3a2b98aa89441c63d8
Miroslava Hreskova <hroncovam@yahoo.com>
2024-12-03 16:48:38 +0100

    Hide Contact page

--- app/routes/components/Header.tsx
+++ app/routes/components/Header.tsx
@@ -31,9 +31,9 @@ export default function Header() {
             <li>
               <a href="/portfolio">Portfolio</a>
             </li>
-            <li>
+            {/* <li>
               <a href="/contact">Contact</a>
-            </li>
+            </li> */}
           </ul>
           <button
             className={styles.hamburger}
