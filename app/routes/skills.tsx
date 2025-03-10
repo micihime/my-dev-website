61e846475f0280d4b9120437cb44c223d3017a21
Miroslava Hreskova <hroncovam@yahoo.com>
2025-03-10 21:55:45 +0100

    Skills text rework

--- app/routes/skills.tsx
+++ app/routes/skills.tsx
@@ -21,22 +21,23 @@ export default function Index() {
   return (
     <div className="content">
       <p>
+        With a background in both frontend and backend development, I enjoy
+        creating intuitive and efficient web applications. My journey started in
+        .NET and C#, but my passion for user experience led me to frontend
+        development with React.
+      </p>
+      <SkillList />
+      <p>
+        Beyond technical skills, my experience in Toastmasters has strengthened
+        my ability to communicate complex ideas clearly, making me an effective
+        collaborator in any team.
+      </p>
+      {/* <p>
         I love web development because it allows me to combine my logical side
         with my creative side. Solving technical challenges and seeing the
         results come to life in an interactive way is very rewarding. It&apos;s
         a mix of problem-solving and design that keeps me engaged.
-      </p>
-      <h3>Technical Stack</h3>
-      <SkillList />
-      <p>
-        With a background in both frontend and backend development, I enjoy
-        creating intuitive and efficient web applications. My journey started in
-        .NET and C#, but my passion for user experience led me to frontend
-        development with React. I value clean code, maintainable architecture,
-        and seamless user interactions. Beyond technical skills, my experience
-        in Toastmasters has strengthened my ability to communicate complex ideas
-        clearly, making me an effective collaborator in any team.
-      </p>
+      </p> */}
     </div>
   );
 }
