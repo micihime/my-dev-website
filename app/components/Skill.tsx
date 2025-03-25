8187d58aefb32e720b99ad37a64bbd8c7540ffdc
Miroslava Hreskova <hroncovam@yahoo.com>
2025-03-25 20:11:04 +0100

    Improving skill styling

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -1,11 +1,17 @@
 interface SkillProps {
   title: string;
   details: string;
+  icon?: string;
 }
 
-export default function Skill({ title, details }: SkillProps) {
+export default function Skill({ title, details, icon }: SkillProps) {
   return (
     <li>
+      {icon && (
+        <div className="skill-icon">
+          <i className={`fa ${icon}`}></i>
+        </div>
+      )}
       <h3>{title}:</h3>
       <p>{details}</p>
     </li>
