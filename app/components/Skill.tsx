ebdbf0571092f035ad81293cbb034cfe6639fd6d
Miroslava Hreskova <hroncovam@yahoo.com>
2025-03-25 20:15:57 +0100

    Adding icons

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -1,17 +1,13 @@
 interface SkillProps {
   title: string;
   details: string;
-  icon?: string;
+  icon?: React.ReactNode;
 }
 
 export default function Skill({ title, details, icon }: SkillProps) {
   return (
     <li>
-      {icon && (
-        <div className="skill-icon">
-          <i className={`fa ${icon}`}></i>
-        </div>
-      )}
+      {icon && <div className="skill-icon">{icon}</div>}
       <h3>{title}:</h3>
       <p>{details}</p>
     </li>
--- app/components/SkillList.tsx
+++ app/components/SkillList.tsx
@@ -1,5 +1,6 @@
 import Skill from "~/components/Skill";
 import styles from "./SkillList.module.css";
+import { IconServer, IconCode, IconUsers } from "@tabler/icons-react";
 
 export default function SkillList() {
   return (
@@ -8,14 +9,17 @@ export default function SkillList() {
         <Skill
           title="Backend"
           details=".NET, C#, SQL, TypeScript / JavaScript"
+          icon={<IconServer size={24} />}
         />
         <Skill
           title="Frontend"
           details="React, Remix, TypeScript / JavaScript, HTML, CSS"
+          icon={<IconCode size={24} />}
         />
         <Skill
           title="Soft skills"
           details="Problem-solving, adaptability, communication, teamwork"
+          icon={<IconUsers size={24} />}
         />
       </ul>
     </div>
